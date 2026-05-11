86328435d5465882452ba76b412d09cd4266cbb1
protestor6 <p7777-3333@qq.com>
2026-05-11 18:50:44 +0800

    把前端登录框改的更好看，修正退出登录不销毁session的问题

--- src/com/sss/servlet/LogoutServlet.java
+++ src/com/sss/servlet/LogoutServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    HttpSession session = request.getSession();
-	    session.invalidate(); // 销毁登录态
-	    // 用绝对路径跳转到根目录的 login.html
-	    response.sendRedirect(request.getContextPath() + "/login.html");
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // 获取当前会话
+        HttpSession session = request.getSession();
+        // 销毁会话，清除登录态
+        session.invalidate();
+        // 用绝对路径跳转到根目录的 login.html
+        response.sendRedirect(request.getContextPath() + "/login.html");
+    }
 }
