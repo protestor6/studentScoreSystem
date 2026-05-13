9d601a5c765cc066bff2ca332e1bba82435994b3
protestor6 <p7777-3333@qq.com>
2026-05-13 18:16:20 +0800

    小修改
    1.把ScoreQueryServlet改为StudentScoreQueryServlet，以免与其他模块的成绩查询冲突
    2.在已有的score(total_score)索引基础上，给学生表和成绩表的学号也加上，加速查询

--- src/com/sss/servlet/StudentScoreQueryServlet.java
+++ src/com/sss/servlet/StudentScoreQueryServlet.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-public class ScoreQueryServlet extends HttpServlet {
+public class StudentScoreQueryServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private StudentScoreService StudentScoreService = new StudentScoreServiceImpl();
 
